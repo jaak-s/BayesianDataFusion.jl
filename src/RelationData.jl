937bb2ba43bb69153cccbc8e69978b0c4ed68001
Jaak Simm <jaak.simm@gmail.com>
2015-01-23 17:52:36 +0100

    new relational data object

--- src/RelationData.jl
+++ src/RelationData.jl
@@ -1,5 +1,7 @@
 using DataFrames
 
+typealias SparseMatrix SparseMatrixCSC{Float64, Int64} 
+
 type Entity{FT,R}
   F::FT
   relations::Vector{R}
@@ -9,13 +11,34 @@ end
 hasFeatures(entity::Entity) = ! isempty(entity.F)
 
 type Relation
-  data::Vector{SparseMatrixCSC{Float64,Int64}} ## sparse matrix for each mode
+  data::Vector{SparseMatrix} ## sparse matrix for each mode
   entities::Vector{Entity}
   name::String
-  Relation(data::SparseMatrixCSC{Float64,Int64}, name::String) = new({data,data'}, Entity[], name)
+
+  test_vec
+  test_ratings
+  mean_rating::Float64
+
+  Relation(data::SparseMatrix, name::String) = new({data,data'}, Entity[], name)
 end
 
-type RelationData{XT,YT}
+import Base.size
+size(r::Relation) = size(r.data[1])
+
+type RelationData
+  entities::Vector{Entity}
+  relations::Vector{Relation}
+  function RelationData(Am::SparseMatrix; feat1=(), feat2=(), entity1="compound", entity2="protein", relation="IC50")
+    r  = Relation( Am, relation )
+    e1 = Entity{typeof(feat1),Relation}( feat1, [r], entity1 )
+    e2 = Entity{typeof(feat2),Relation}( feat2, [r], entity2 )
+    push!(r.entities, e1)
+    push!(r.entities, e2)
+    return new( {e1, e2}, {r} )
+  end
+end
+
+type RelationDataX{XT,YT}
   F::XT
   Ft::XT
   Am::YT
@@ -25,13 +48,12 @@ type RelationData{XT,YT}
   probe_vec
   ratings_test
   mean_rating::Float64
-  RelationData(F, Am) = new(F, F', Am, Am', size(Am,1), size(Am,2))
+  RelationDataX(F, Am) = new(F, F', Am, Am', size(Am,1), size(Am,2))
 end
 
-function normalizeFeatures!(data::RelationData)
-  diagsq  = sqrt(vec( sum(data.F .^ 2,1) ))
-  data.F  = data.F * spdiagm(1.0 ./ diagsq)
-  data.Ft = data.F'
+function normalizeFeatures!(entity::Entity)
+  diagsq  = sqrt(vec( sum(entity.F .^ 2,1) ))
+  entity.F  = entity.F * spdiagm(1.0 ./ diagsq)
   return
 end
 
@@ -55,13 +77,13 @@ function load_mf1c(;ic50_file     = "chembl_19_mf1c/chembl-IC50-346targets.csv",
   num_p, num_m = size(Am)
   
   ## creating data object
-  data = RelationData{typeof(F), typeof(Am)}(F, Am)
-  data.probe_vec    = probe_vec
-  data.ratings_test = data.probe_vec[:,3] .< log10(200)
-  data.mean_rating  = sum(Am) / size(X,1)
+  data = RelationData(Am, feat1 = F)
+  data.relations[1].test_vec     = probe_vec
+  data.relations[1].test_ratings = data.relations[1].test_vec[:,3] .< log10(200)
+  data.relations[1].mean_rating  = sum(Am) / size(X,1)
 
   if normalize_feat
-    normalizeFeatures!(data)
+    normalizeFeatures!(data.entities[1])
   end
   
   return data
