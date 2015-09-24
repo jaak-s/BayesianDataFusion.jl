26b7b8a26a57bd0b3eb4e84992fb0c1b69d17ab9
Jaak Simm <jaak.simm@gmail.com>
2015-09-24 18:28:46 +0200

    removed At_mul_B file as v0.4 has it now

--- src/BayesianDataFusion.jl
+++ src/BayesianDataFusion.jl
@@ -2,7 +2,6 @@ module BayesianDataFusion
 
 include("ROC.jl")
 include("normal_wishart.jl")
-#include("At_mul_B.jl")
 include("RelationData.jl")
 include("parallel_matrix.jl")
 include("sparsebin_csr.jl")
