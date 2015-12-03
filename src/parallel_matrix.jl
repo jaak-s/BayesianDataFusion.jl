b47b9e335dbc0a2284124a2cbed7faed1aad67ed
Jaak Simm <jaak.simm@gmail.com>
2015-12-03 18:49:40 +0100

    added getindex for Julia v0.3

--- src/parallel_matrix.jl
+++ src/parallel_matrix.jl
@@ -24,7 +24,7 @@ SparseBinMatrix(rows::Vector{Int32}, cols::Vector{Int32}) = SparseBinMatrix(maxi
 SparseBinMatrix(rows::Vector{Int64}, cols::Vector{Int64}) = SparseBinMatrix(convert(Vector{Int32}, rows), convert(Vector{Int32}, cols))
 
 import Base.getindex
-function getindex(sbm::SparseBinMatrix, rows::Vector{Bool}, cols::Colon)
+function getindex(sbm::SparseBinMatrix, rows::Vector{Bool})
   length(rows) == sbm.m || throw(DimensionMismatch("length(rows) must equal size(sbm,1)"))
   idx = rows[sbm.rows]
   colidx = sbm.cols[idx]
@@ -32,6 +32,14 @@ function getindex(sbm::SparseBinMatrix, rows::Vector{Bool}, cols::Colon)
   rowidx = rsum[ sbm.rows[idx] ]
   out = SparseBinMatrix(sum(rows), sbm.n, one(Int32):convert(Int32,sum(rows)), sbm.nrange, rowidx, colidx)
   return out
+end
+
+function getindex(sbm::SparseBinMatrix, rows::Vector{Bool}, cols::Colon)
+  return getindex(sbm, rows)
+end
+
+function getindex(sbm::SparseBinMatrix, rows::Vector{Bool}, cols::UnitRange{Int64})
+  return getindex(sbm, rows)
 end
 
 import Base.ndims
