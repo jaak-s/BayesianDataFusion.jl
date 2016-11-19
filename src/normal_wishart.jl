65308022c3cbe435d691d196d22c0b4c4ee548fe
Jaak Simm <jaak.simm@gmail.com>
2016-11-19 15:30:56 +0200

    for julia-0.5: make symmetric manually

--- src/normal_wishart.jl
+++ src/normal_wishart.jl
@@ -9,11 +9,12 @@ immutable NormalWishart <: Distribution
     zeromean::Bool
     mu::Vector{Float64}
     kappa::Float64
-    Tchol::Cholesky{Float64}  # Precision matrix (well, sqrt of one)
+    #Tchol::Cholesky{Float64}  # Precision matrix (well, sqrt of one)
+    T::Matrix{Float64}
     nu::Float64
 
     function NormalWishart(mu::Vector{Float64}, kappa::Real,
-                                  Tchol::Cholesky{Float64}, nu::Real)
+                                  T::Matrix{Float64}, nu::Real)
         # Probably should put some error checking in here
         d = length(mu)
         zmean::Bool = true
@@ -23,24 +24,19 @@ immutable NormalWishart <: Distribution
                 break
             end
         end
-        @compat new(d, zmean, mu, Float64(kappa), Tchol, Float64(nu))
+        @compat new(d, zmean, mu, Float64(kappa), full(Symmetric(T)), Float64(nu))
     end
 end
 
 function NormalWishart(mu::Vector{Float64}, kappa::Real,
                        T::AbstractMatrix{Float64}, nu::Real)
-    NormalWishart(mu, kappa, cholfact(T), nu)
-end
-
-function NormalWishart(mu::Vector{Float64}, kappa::Real,
-                       T::PDMat, nu::Real)
-    NormalWishart(mu, kappa, T.chol, nu)
+    NormalWishart(mu, kappa, full(T), nu)
 end
 
 import Distributions.rand
 
 function rand(nw::NormalWishart)
-    Lam = rand(Wishart(nw.nu, nw.Tchol))
+    Lam = rand(Wishart(nw.nu, nw.T))
     mu = rand(MvNormal(nw.mu, full(inv(Symmetric(Lam))) ./ nw.kappa))
     return (mu, Lam)
 end
